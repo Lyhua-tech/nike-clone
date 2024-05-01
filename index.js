e7239e52d24b4546a86736d72afcd8ed058b2c86
sreang lyhour <slyhour69@gmail.com>
2024-05-01 18:19:45 +0700

    Add upperfooter

--- index.js
+++ index.js
@@ -8,3 +8,16 @@ function plusSlides(n) {
   currentSlide = Math.max(0, Math.min(currentSlide + n, images.length - 1)); 
   carousel.scrollTo({ left: currentSlide * imageWidth, behavior: 'smooth' });
 }
+
+function hoverShow(){
+  let whiteblock = document.getElementById('white-block')
+  let footersec = document.getElementById('footer-sec')
+  whiteblock.classList.add('hidden')
+  footersec.classList.add('top-[183px]')
+}
+function hoverhide(){
+  let whiteblock = document.getElementById('white-block')
+  let footersec = document.getElementById('footer-sec')
+  whiteblock.classList.remove('hidden')
+  footersec.classList.remove('top-[183px]')
+}
